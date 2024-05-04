d4a698d43bd588ff2731d61b2b9157813b185d9c
Máté Andrea <74617712+mt-andrea@users.noreply.github.com>
2024-05-04 17:08:26 +0200

    install Newtonsoft install RestSharp implement Button_Regisztracio() create RegisterUser() create UserRegistrationData add .gitignore

--- AntiTrouble-Defender/AntiTrouble-Defender/Register.xaml.cs
+++ AntiTrouble-Defender/AntiTrouble-Defender/Register.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace AntiTrouble_Defender
 {
@@ -24,10 +26,26 @@ namespace AntiTrouble_Defender
             InitializeComponent();
         }
 
-
-        public void Button_Regisztracio(object sender, RoutedEventArgs e)
+        public class UserRegistrationData
         {
-            // TODO
+            public string userName { get; set; }
+            public string password { get; set; }
+        }
+
+        public async void Button_Regisztracio(object sender, RoutedEventArgs e)
+        {
+            string userName = Felhasznalonev.Text;
+            string password = Jelszo.Password;
+
+            bool success = await RegisterUser(userName, password);
+
+            if (success)
+            {
+                MessageBox.Show("User registered successfully!");
+                Felhasznalonev.Clear();
+                Jelszo.Clear();
+                JelszoUjra.Clear();
+            }
         }
 
         public void Button_Vissza(object sender, RoutedEventArgs e)
@@ -38,6 +56,42 @@ namespace AntiTrouble_Defender
         public void JelszoValtozas(object sender, RoutedEventArgs e)
         {
             // TODO
+        }
+        public async Task<bool> RegisterUser(string userName, string password)
+        {
+            string apiUrl = "http://localhost/API/registration.php";
+
+            UserRegistrationData registrationData = new UserRegistrationData();
+            registrationData.userName = userName;
+            registrationData.password = password;
+
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonData = JsonConvert.SerializeObject(registrationData);
+                StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(responseContent);
+
+                    if (result.error == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show(result.message.ToString());
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("An error occurred while registering the user.");
+                    return false;
+                }
+            }
         }
     }
 }
