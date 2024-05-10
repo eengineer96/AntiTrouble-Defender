a8a7d9267958c9c329b0f1e050ff9d4107890c64
kallaiviktor <kallaivikti@gmail.com>
2024-05-10 11:30:05 +0200

    Added "Defenderdatabase" class Added "CloseConnection" function to the database class Added new column(Password) to the database

---
+++ AntiTrouble-Defender/AntiTrouble-Defender/DefenderDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Data.SQLite;
+
+namespace AntiTrouble_Defender
+{
+    public class DefenderDatabase
+    {
+        private static readonly string connectionString = "Server=127.0.0.1;Database=Defender; User ID=root;Password=;";
+
+        private void CloseConnection(SQLiteConnection connection)
+        {
+            if(connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
