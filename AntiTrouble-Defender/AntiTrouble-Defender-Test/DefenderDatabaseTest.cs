96d5dd75bccdac77e2a00c1f63de3251f0b96567
SziliCs <yzmgcq@gmail.com>
2024-05-23 21:23:49 +0200

    Writing first unit test for Database.cs.

---
+++ AntiTrouble-Defender/AntiTrouble-Defender-Test/DefenderDatabaseTest.cs
@@ -0,0 +1,25 @@
+﻿using AntiTrouble_Defender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace AntiTrouble_Defender_Test
+{
+    public class DefenderDatabaseTest
+    {
+        DefenderDatabase db = new DefenderDatabase();
+
+        [WpfTheory]
+        [InlineData("Teszt Elek", 9)]
+        [InlineData("Szilveszter", 10)]
+        public void GetUserID_test(string username, int expected)
+        {
+            int result = db.GetUserID(username);
+            Assert.Equal(result, expected);
+        }
+    }
+}
